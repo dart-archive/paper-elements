fbe3b294e97f7a6baebc2a8a32303216bc8021ec
John Messerly <jmesserly@google.com>
2014-06-24 15:51:56 -0700

    add roboto.html

---
+++ lib/roboto.dart
@@ -0,0 +1,10 @@
+// DO NOT EDIT: auto-generated with core_elements/tool/generate_dart_api.dart
+
+/// Dart API for the polymer element `roboto`.
+library core_elements.roboto;
+
+import 'dart:html';
+import 'dart:js' show JsArray;
+import 'package:web_components/interop.dart' show registerDartType;
+import 'package:polymer/polymer.dart' show initMethod;
+
